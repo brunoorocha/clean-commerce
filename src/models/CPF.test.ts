737eece07842d34b20bff2aa06c1b70d1f20a802
Bruno Rocha da Silva <bruno.silva@willbank.com.br>
2021-10-15 09:43:09 -0300

    feat: add unit tests to CPF

---
+++ src/models/CPF.test.ts
@@ -0,0 +1,31 @@
+import CPF from "./CPF"
+
+describe("CPF Model", () => {
+    test("Should not accept an empty CPF", () => {
+        expect(() => CPF.create("")).toThrow(new RangeError("A CPF should have exactly 11 digits."))
+    })
+
+    test("Should not accept a CPF with letters", () => {
+        expect(() => CPF.create("123abc45678")).toThrow(new Error("A CPF must have numbers only."))
+    })
+
+    test("Should not accept a CPF with less than 11 digits", () => {
+        expect(() => CPF.create("123.456.789-0")).toThrow(new RangeError("A CPF should have exactly 11 digits."))
+    })
+
+    test("Should not accept a CPF with only the same digit", () => {
+        expect(() => CPF.create("111.111.111-11")).toThrow(new Error('This CPF is not valid.'))
+    })
+
+    test("Should not accept a CPF that breaks CPF validation rules", () => {
+        expect(() => CPF.create("123.456.789-00")).toThrow(new Error('This CPF is not valid.'))
+    })
+
+    test("Should accept a valid CPF without CPF mask", () => {
+        expect(() => CPF.create("025.789.690-27")).not.toThrowError()
+    })
+
+    test("Should accept a valid CPF with CPF mask", () => {
+        expect(() => CPF.create("025.789.690-27")).not.toThrowError()
+    })
+})
